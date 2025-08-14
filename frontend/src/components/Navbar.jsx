e263a2cd92502c0b287ab6a0878fd81425c2b78c
mds_sayyad <155095438+saanisayyad@users.noreply.github.com>
2025-08-14 13:31:15 +0530

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className=" flex items-center rounded-full justify-between mt-10 py-4 px-10 gap-5">
-      <Link to="/joke">
+      <Link to="/api/joke">
         <div className='bg-white/10 px-3 py-2 text-center rounded-full hover:scale-105 duration-300 cursor-pointer text-white/70'>
           Joke
         </div>
       </Link>
-      <Link to="/shayari">
+      <Link to="/api/shayari">
         <div className='bg-white/10 px-3 py-2 text-center rounded-full hover:scale-105 duration-300 cursor-pointer text-white/70'>
       Shayari
     </div>
       </Link>
-      <Link to="/quote">
+      <Link to="/api/quote">
         <div className='bg-white/10 px-3 py-2 text-center rounded-full hover:scale-105 duration-300 cursor-pointer text-white/70'>
       Quote
     </div>
